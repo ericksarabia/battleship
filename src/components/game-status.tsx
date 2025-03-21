631f646219f7b3de0af45d520a20d57c8653579b
Erick Sarabia <3ricksarabia@gmail.com>
2025-03-20 21:30:21 -0600

    feat: add grid status component

---
+++ src/components/game-status.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import type { GameState } from "../lib/types";
+
+interface GameStatusProps {
+  gameState: GameState;
+}
+
+const GameStatus: FC<GameStatusProps> = ({ gameState }) => {
+  return (
+    <div className="mt-6 p-4 border rounded-md bg-green-50 text-green-800 flex items-start w-full min-w-[500px]">
+      <div>
+        <p className="font-semibold">
+          Game Status: <span className="font-normal">{gameState.message}</span>
+        </p>
+        <p className="font-semibold">
+          Shots Fired:{" "}
+          <span className="font-normal">{gameState.shotsFired}</span>
+        </p>
+        <p className="font-semibold">
+          Ships Remaining:{" "}
+          <span className="font-normal">
+            {gameState.ships.filter((ship) => ship.hits < ship.size).length} /{" "}
+            {gameState.ships.length}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GameStatus;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,14 @@
 export const GRID_SIZE = 10;
 
+export type Ship = {
+  type: string;
+  size: number;
+  positions: number[][];
+  hits: number;
+};
+
 export type GameState = {
+  ships: Ship[];
   grid: (null | "hit" | "miss")[][];
   shotsFired: number;
   gameOver: boolean;
