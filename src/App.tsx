c9b2432aca7e1351e03384cdbd05ab911fc0114e
Erick Sarabia <3ricksarabia@gmail.com>
2025-03-20 21:37:42 -0600

    feat: update body and root styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Battleship from "./components/battleship";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Home() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex h-full flex-col items-center justify-between">
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm">
+        <h1 className="mb-8 text-center text-4xl font-bold text-green-600">
+          Battleship
+        </h1>
+        <Battleship />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
-
-export default App
